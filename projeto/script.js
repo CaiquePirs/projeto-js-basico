2c670d6d1e75a42a37df74a1d1a2886870b75fdd
Caique Pires <c.pires@ba.estudante.senai.br>
2024-12-09 17:36:30 -0300

    obtendo valor do input e validando para receber somente numeros

--- projeto/script.js
+++ projeto/script.js
@@ -1,2 +1,12 @@
+//variavel para receber o dado digitado do input
+const amount = document.getElementById("amount")
 
+//função para receber o valor digitado
+amount.addEventListener("input", (event)=>{
+    console.log(amount.value)
+    
+    //criando uma expressão regular para receber somente numeros
+    const onlyNumbersRegex = /\D+/g
+    amount.value = amount.value.replace(onlyNumbersRegex, "")
 
+})
