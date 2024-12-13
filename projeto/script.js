05204a4cc17ba1bf4a31157d5405f1c2052c41e7
Caique Pires <c.pires@ba.estudante.senai.br>
2024-12-12 21:40:35 -0300

    obtendo a moeda escolhida

--- projeto/script.js
+++ projeto/script.js
@@ -1,12 +1,41 @@
-//variavel para receber o dado digitado do input
-const amount = document.getElementById("amount")
+//variaveis
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+const form = document.querySelector("form");
 
-//função para receber o valor digitado
-amount.addEventListener("input", (event)=>{
-    console.log(amount.value)
-    
-    //criando uma expressão regular para receber somente numeros
-    const onlyNumbersRegex = /\D+/g
-    amount.value = amount.value.replace(onlyNumbersRegex, "")
+// Cotação das moedas
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.50;
 
-})
+// Função para receber o valor digitado
+amount.addEventListener("input", (event) => {
+    // Criando uma expressão regular para permitir somente números
+    const onlyNumbersRegex = /\D+/g;
+    amount.value = amount.value.replace(onlyNumbersRegex, "");
+});
+
+// Obtendo a moeda selecionada
+form.onsubmit = (event) => {
+    event.preventDefault(); // Corrigido de preventDefalt para preventDefault
+
+    // Switch case para selecionar a moeda escolhida
+    switch (currency.value) {
+        case "USD":
+            convertCurrency(amount.value, USD, "$");
+            break;
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€");
+            break;
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£");
+            break;
+    }
+};
+
+// Função para converter a moeda
+function convertCurrency(amount, price, simbol) {
+
+}
+
+
